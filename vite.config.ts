8efcd5e5c5d94cf5bc2f6cd0be42930936b96639
alibaba <iron2212@aol.com>
2024-11-13 19:15:13 +0100

    Ajout de la propriété homepage dans package.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://alybi.github.io/mycoachingpro/", // Remplace 'my-react-app' par le nom de ton dépôt GitHub
+  base: "/mycoachingpro/", // Remplace 'mycoachingpro' par le nom de ton dépôt GitHub
 });
